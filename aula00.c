a0e205db6327e240ea1332581442abf676e523db
Falcão <gabriel.p.falcao50@gmail.com>
2024-03-04 15:01:50 +0000

    primeira aula

---
+++ aula00.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<assert.h>
+
+void inc(int v) {
+	printf("antes inc %p %d\n", (void *)&v,v);
+	// cada chamada cria uma nova variável em outro endereço da memória
+	// com endereços decrescentes
+	if (v!=0) {
+		inc(v-1);
+	}
+	// queeee
+	if (v!=0) {
+		inc(v-1);
+	}
+	// aqui os endereços são crescentes pois são quando a função volta, quem diria que é uma stack...
+	printf("depois inc %p %d\n", (void *)&v, v);
+}
+
+int main(void) {
+	// o v da main tem endereço diferente ao v da função, mesmo sendo passado como parâmetro
+	int v;
+	printf("fora %p %d\n", (void *)&v,v);
+	v = 3;
+	inc(v);
+	printf("fora %p %d\n", (void *)&v, v);
+	return EXIT_SUCCESS;
+}
