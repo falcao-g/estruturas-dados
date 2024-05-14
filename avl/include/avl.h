f29f963b8c88c7f33e222d3a4d77c8dcd912cf3e
Falcão <gabriel.p.falcao50@gmail.com>
2024-05-13 21:44:00 -0400

    adiciona os arquivos referentes a avl

---
+++ avl/include/avl.h
@@ -0,0 +1,21 @@
+#ifndef __AVL__
+#define __AVL__
+
+typedef int titem;
+
+typedef struct _node {
+    titem item;
+    struct _node *esq;
+    struct _node *dir;
+    int h;
+} tnode;
+
+void avl_insere(tnode **parv, titem reg);
+void avl_remove(tnode **parv, titem reg);
+void avl_destroi(tnode *parv);
+
+void _rd(tnode **pparv);
+void _re(tnode **pparv);
+void _avl_rebalancear(tnode **pparv);
+
+#endif
